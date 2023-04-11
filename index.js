9ebccdd030659735d1a3727a8de1aed9dca454c3
paul-tes <paulasca8462@gmail.com>
2023-04-11 08:43:57 +0300

    error mobile container size is expanded

---
+++ index.js
@@ -0,0 +1,14 @@
+// let btn = document.querySelector('.menue-btn');
+
+// btn.addEventListener('click', function(event) {
+//     let logo = document.querySelector('.logo')
+//     logo.classList.add('hidden');
+
+//     alert("I am clicked")
+
+
+//     let mob_menue = document.querySelector('.mobile-menue-active');
+//     mob_menue.classList.add('display-menue')
+
+//     let except_headaer_section = document.querySelector('..container > *:not(.header, .section)');
+// });
